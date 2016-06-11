50cdb1ce226637df581f56b095a90301809ddaaf
Gareth Gomersall <ggomersall@gmail.com>
2016-06-11 16:05:52 +0100

    created playlist struct for data from MusicLibrary file

---
+++ Algorythm/Playlist.swift
@@ -0,0 +1,20 @@
+//
+//  Playlist.swift
+//  Algorythm
+//
+//  Created by dev on 11/06/2016.
+//  Copyright © 2016 GarethG. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Playlist {
+    var title: String?
+    var description: String?
+    var icon: UIImage?
+    var largeIcon: UIImage?
+    var artists: [String] = []
+    var backgroundColor: UIColor = UIColor.clearColor()
+    
+}
