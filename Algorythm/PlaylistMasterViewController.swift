f72c074ade39b2a9c6782e6f2d0e0c3709ee967e
Gareth Gomersall <ggomersall@gmail.com>
2016-06-11 15:35:25 +0100

    Initial Commit for Algorythm

---
+++ Algorythm/PlaylistMasterViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Algorythm
+//
+//  Created by dev on 11/06/2016.
+//  Copyright © 2016 GarethG. All rights reserved.
+//
+
+import UIKit
+
+class PlaylistMasterViewController: UIViewController {
+
+    // Here we setup up the button
+    @IBOutlet weak var aButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        aButton.setTitle("Press Me!", forState: .Normal)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
+
